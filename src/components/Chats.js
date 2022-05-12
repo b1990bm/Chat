55cc4b5d4f3aa0db1f6a6e1da30a2dea241c8d91
Behnam <b1990bm@gmail.com>
2022-05-12 23:00:49 +0430

    chat 1

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,12 +1,29 @@
 import React from 'react';
-
+import { auth } from '../firebase';
+import { useHistory } from 'react-router-dom/';
+import {ChatEngine} from "react-chat-engine";
 //Styles
 import styles from "./Chats.module.css";
 import Navbar from './Navbar';
 const Chats = () => {
+
+    //History
+    const history=useHistory();
+
+    //logout func
+    const logouthandeler=async()=>{
+        await auth.signOut();
+        history.push("/");
+    }
+
     return (
         <div className={styles.container}>
-            <Navbar />
+            <Navbar logouthandeler={logouthandeler}/>
+            <ChatEngine 
+                height= "calc(100vh - 50px)"
+                projectId=""
+
+            />
         </div>
     );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 //Styles
 import styles from "./Navbar.module.css";
-const Navbar = () => {
+const Navbar = ({logouthandeler}) => {
     return (
         <div className={styles.container}>
             <div className={styles.name}>
                 Chat!
             </div>
-            <div className={styles.logout}>
+            <div className={styles.logout} onClick={logouthandeler}>
                 Logout
             </div>
         </div>
